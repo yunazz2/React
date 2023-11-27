7b6b89e902b81720195c2f17b400f214fcf299a0
yunazz2 <z____yn@naver.com>
2023-11-27 16:05:51 +0900

    ⛄

---
+++ practice/01.Component/05.event/event-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Product from './component/Product';
+
+function App() {
+  return (
+    <>
+      <Product/>
+    </>
+  );
+}
+
+export default App;
