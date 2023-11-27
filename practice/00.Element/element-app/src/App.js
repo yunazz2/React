13520b22aa386ecce6d9d489ec773762920ce11d
yunazz2 <z____yn@naver.com>
2023-11-27 10:51:41 +0900

    ⛄

---
+++ practice/00.Element/element-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+// 클래스형 컴포넌트
+class App extends React.Component {
+  // React 엘리먼트 생성
+  const element = React.createElement('h1', null, 'Hello Element~!')
+
+  render() {
+
+  }
+}
+
+export default App;
