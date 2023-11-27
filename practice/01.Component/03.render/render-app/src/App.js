27017b2f7fcbc6156cf58d9eb760a166b354b745
yunazz2 <z____yn@naver.com>
2023-11-27 15:41:30 +0900

    ⛄

---
+++ practice/01.Component/03.render/render-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LifecycleExample from './component/LifeCycleExample';
+
+function App() {
+    return (
+      <LifecycleExample/>
+    );
+}
+
+export default App;
