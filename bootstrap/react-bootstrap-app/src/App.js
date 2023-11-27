79da9356d54c4e5a4d5640148fd9d638e6bbb977
yunazz2 <z____yn@naver.com>
2023-11-27 16:54:44 +0900

    ⛄

---
+++ bootstrap/react-bootstrap-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello, Bootstrap!</h1>
+      <Button variant="primary">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+    </Container>
+  );
+}
+
+export default App;
